e26c8bb3dc09b28c52e358217b09ba7008bf2e6c
hdbshshshshs0-commits <hdbshshshshs0@gmail.com>
2026-07-07 15:21:26 +0500

    Update auth_user_model.dart

--- lib/features/auth/data/models/auth_user_model.dart
+++ lib/features/auth/data/models/auth_user_model.dart
@@ -6,4 +6,18 @@ class AuthUserModel {
   final UserRole role;
 
   const AuthUserModel({required this.phone, required this.name, required this.role});
+
+  Map<String, dynamic> toJson() => {
+        'phone': phone,
+        'name': name,
+        'role': role.name,
+      };
+
+  factory AuthUserModel.fromJson(Map<String, dynamic> json) {
+    return AuthUserModel(
+      phone: json['phone'] as String,
+      name: json['name'] as String,
+      role: UserRole.values.byName(json['role'] as String),
+    );
+  }
 }
